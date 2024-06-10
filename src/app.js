8dce466ac66de838c76f22cf752d9d22cc2acda8
EvNolasco <135380253+EvNolasco@users.noreply.github.com>
2024-06-10 18:39:04 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import productsRouter from './routes/products.js';
 import cartsRouter from './routes/carts.js';
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
